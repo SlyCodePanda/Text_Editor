74bda3190e26d7e3c4f50f34d3b769c371776e9e
SlyCodePanda <reneemarsland@hotmail.com>
2019-05-09 16:45:08 +0930

    adding import changes to gui file

--- gui.py
+++ gui.py
@@ -1,9 +1,20 @@
 import sys
-from PyQt4 import QtGui, QtCore
-from PyQt4.QtCore import Qt
+
+try:
+    Qt.QtCore import QRegExp
+except:
+    from PyQt4 import *
+    from PyQt4.QtCore import *
+
 import syntaxHighlighting
  
 class Main(QtGui.QMainWindow):
+
+    """
+    Get the stylesheet for Dracual:
+    https://draculatheme.com/qtcreator/
+    Need to do this in designer i think so maybe try doing it on home computer.
+    """
  
     def __init__(self, parent = None):
         QtGui.QMainWindow.__init__(self,parent)
@@ -13,7 +24,8 @@ class Main(QtGui.QMainWindow):
     def initUI(self):
  
         # x and y coordinates on the screen, width, height
-        self.setGeometry(300,300,1030,800)
+        # ToDo: This doesn't seem to be working? Still appears as a shitty little tiny window...
+        self.setGeometry(500,500,1030,800)
         self.setWindowTitle("Text Editor")
  
 def main():
