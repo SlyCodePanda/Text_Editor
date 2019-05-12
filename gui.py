8e0077a66634b8551b62e55c048ab1007f160537
Renee Marsland <reneemarsland@hotmail.com>
2019-05-12 14:26:26 +0930

    Fixing gui.py adding ui file
    Created a .ui file to build the ui and create the stylesheet.

--- gui.py
+++ gui.py
@@ -1,49 +1,68 @@
 import sys
 
 try:
-    Qt.QtCore import QRegExp
+    from Qt.QtCore import QRegExp
 except:
-    from PyQt4 import *
-    from PyQt4.QtCore import *
+    from PyQt4 import QtCore, QtGui, uic
 
 import syntaxHighlighting
- 
-class Main(QtGui.QMainWindow):
 
-    """
-    Get the stylesheet for Dracual:
-    https://draculatheme.com/qtcreator/
-    Need to do this in designer i think so maybe try doing it on home computer.
-    """
- 
-    def __init__(self, parent = None):
-        QtGui.QMainWindow.__init__(self,parent)
- 
-        self.initUI()
- 
-    def initUI(self):
- 
-        # x and y coordinates on the screen, width, height
-        # ToDo: This doesn't seem to be working? Still appears as a shitty little tiny window...
-        self.setGeometry(500,500,1030,800)
-        self.setWindowTitle("Text Editor")
- 
+# class Main(QtGui.QMainWindow):
+#
+#     """
+#     Get the stylesheet for Dracual:
+#     https://draculatheme.com/qtcreator/
+#     Need to do this in designer i think so maybe try doing it on home computer.
+#     """
+#
+#     def __init__(self, parent = None):
+#         QtGui.QMainWindow.__init__(self,parent)
+#
+#         self.initUI()
+#
+#     def initUI(self):
+#
+#         # x and y coordinates on the screen, width, height
+#         # ToDo: This doesn't seem to be working? Still appears as a shitty little tiny window...
+#         self.setGeometry(500,500,1030,800)
+#         self.setWindowTitle("Text Editor")
+
+
+# Window inherits from the Qt widget 'MainWindow'
+class Window(QtGui.QMainWindow):
+
+    # Any core application stuff goes in the 'init' function.
+    def __init__(self, parent=None):
+        # Set up the user interface from Designer.
+        self.ui = uic.loadUi("textEditor_GUI.ui")
+
+        # Starting point: 50, 50. And 500 pixels wide, 300 pixels tall.
+        #self.ui.setGeometry(100, 100, 500, 300)
+        self.ui.setWindowTitle("Meow Text v02")
+        #self.ui.setWindowIcon(QtGui.QIcon('cat-black.png'))
+
+        highlight = syntaxHighlighting.PythonHighlighter(self.ui.textWindow.document())
+        # texter.show()
+
+        # Load test file.
+        infile = open('test.py', 'r')
+        self.ui.textWindow.setPlainText(infile.read())
+
+        self.ui.show()
+
+
 def main():
- 
-    app = QtGui.QApplication([])
-    texter = QtGui.QPlainTextEdit()
 
-    highlight = syntaxHighlighting.PythonHighlighter(texter.document())
-    texter.show()
+    app = QtGui.QApplication(sys.argv)
+    GUI = Window()
 
-    infile = open('test.py', 'r')
-    texter.setPlainText(infile.read())
+
  
     #main = Main()
     #main.show()
- 
+
     #sys.exit(texter.exec_())
-    app.exec_()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
