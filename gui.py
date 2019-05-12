8e53242f6689075fc5dbe9cb81c817477e335d57
Renee Marsland <reneemarsland@hotmail.com>
2019-05-12 14:29:38 +0930

    Update gui.py
    I shamefully forgot to delete commented out crap..

--- gui.py
+++ gui.py
@@ -7,26 +7,6 @@ except:
 
 import syntaxHighlighting
 
-# class Main(QtGui.QMainWindow):
-#
-#     """
-#     Get the stylesheet for Dracual:
-#     https://draculatheme.com/qtcreator/
-#     Need to do this in designer i think so maybe try doing it on home computer.
-#     """
-#
-#     def __init__(self, parent = None):
-#         QtGui.QMainWindow.__init__(self,parent)
-#
-#         self.initUI()
-#
-#     def initUI(self):
-#
-#         # x and y coordinates on the screen, width, height
-#         # ToDo: This doesn't seem to be working? Still appears as a shitty little tiny window...
-#         self.setGeometry(500,500,1030,800)
-#         self.setWindowTitle("Text Editor")
-
 
 # Window inherits from the Qt widget 'MainWindow'
 class Window(QtGui.QMainWindow):
@@ -41,8 +21,8 @@ class Window(QtGui.QMainWindow):
         self.ui.setWindowTitle("Meow Text v02")
         #self.ui.setWindowIcon(QtGui.QIcon('cat-black.png'))
 
+        # Load the highlighter
         highlight = syntaxHighlighting.PythonHighlighter(self.ui.textWindow.document())
-        # texter.show()
 
         # Load test file.
         infile = open('test.py', 'r')
@@ -56,12 +36,6 @@ def main():
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
 
-
- 
-    #main = Main()
-    #main.show()
-
-    #sys.exit(texter.exec_())
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
