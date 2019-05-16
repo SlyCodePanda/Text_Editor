9d0e540df4f6a99b731eb4a88c1d558d8fcd67f9
Renee Marsland <reneemarsland@hotmail.com>
2019-05-17 03:37:35 +0930

    functioning test text highlighting
    Finally have a loaded file working and the ability to have syntax highlighting work when writing in the text field.

--- gui.py
+++ gui.py
@@ -13,30 +13,47 @@ class Window(QtGui.QMainWindow):
 
     # Any core application stuff goes in the 'init' function.
     def __init__(self, parent=None):
+        super(Window, self).__init__()
+
+        ###############
+        # Main Window #
+        ###############
+
         # Set up the user interface from Designer.
         self.ui = uic.loadUi("textEditor_GUI.ui")
-
-        # Starting point: 50, 50. And 500 pixels wide, 300 pixels tall.
-        #self.ui.setGeometry(100, 100, 500, 300)
         self.ui.setWindowTitle("Meow Text v02")
-        #self.ui.setWindowIcon(QtGui.QIcon('cat-black.png'))
 
-        # Load the highlighter
-        highlight = syntaxHighlighting.PythonHighlighter(self.ui.textWindow.document())
-
-        # Load test file.
-        infile = open('test.py', 'r')
-        self.ui.textWindow.setPlainText(infile.read())
-
+        # Load UI.
         self.ui.show()
 
+
+def load_file(file, editor):
+    """
+    Loads a file into the plain text editor.
+    :param file: The name of the file you want to load.
+    :return: None.
+    """
+    # Load test file.
+    infile = open('test.py', 'r')
+    editor.setPlainText(infile.read())
 
 def main():
 
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
 
+    ###############
+    # Text Editor #
+    ###############
+
+    # Build the text editor field and load highlighter into it.
+    editor = QtGui.QPlainTextEdit()
+    highlighter = syntaxHighlighting.PythonHighlighter(editor.document())
+    GUI.ui.gridLayout.addWidget(editor)
+    load_file('test.py', editor)
+
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
